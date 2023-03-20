9ff097647924911649f20fc79f55bcd4fde0a9ab
MER <wjang07@gmail.com>
2023-03-20 10:36:54 +0900

    테트리스 진행중

--- src/components/gameTetris_components/components/displays/Controller.js
+++ src/components/gameTetris_components/components/displays/Controller.js
@@ -2,18 +2,35 @@
 import React, {useContext, useEffect } from "react"
 import styled, {css} from "styled-components"
 
+import { Div } from "../../../../styles/Div"
+import { Img } from "../../../../styles/Img"
+
 
 // ===== import components =====
 const DisplayDiv =styled.div`
     height :60px;
-
+`
+const ArrowBtnImg =styled.img`
+    transform:rotate(${props => props.deg || "0deg"});
 `
 // ===== import utils =====
 
-const Controller =({text})=>{
+const Controller =({props})=>{
+    
 
     return(
-    <DisplayDiv>{text}</DisplayDiv>
+    <DisplayDiv>
+        <Div>
+            <Div flex_direction="column">
+                <Div  width="180px" justify_content="space-around" >
+                    <ArrowBtnImg deg="90deg" src="img_srcs/game_img/tetris/jelly/asset/arrowBtnImg.png"/>
+                    <ArrowBtnImg deg="270deg" src="img_srcs/game_img/tetris/jelly/asset/arrowBtnImg.png"/>
+                </Div>
+                <ArrowBtnImg src="img_srcs/game_img/tetris/jelly/asset/arrowBtnImg.png"/>
+            </Div>
+            <Img src="img_srcs/game_img/tetris/jelly/asset/rotateBtnImg.png"/>
+        </Div>
+    </DisplayDiv>
     )
 }
 
